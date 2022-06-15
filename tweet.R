1cbb4ae6a8d25f4ad8c14d8f801a85675bb9220f
Nur Andi Setiabudi <nurandi@users.noreply.github.com>
2022-06-16 04:04:28 +0700

    delete unused lines

--- tweet.R
+++ tweet.R
@@ -8,17 +8,15 @@ mongo_movie <- mongo(collection = 'id',
                      url = conn_string)
 
 twitter_token <- rtweet::create_token(
-  app              = "new-movie-bot",
+  app              = Sys.getenv("TWITTER_APP_NAME"),
   consumer_key     = Sys.getenv("TWITTER_CONSUMER_API_KEY"),
   consumer_secret  = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
   access_token     = Sys.getenv("TWITTER_ACCESS_TOKEN"),
-  access_secret    = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+  access_secret    = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
+  set_renv         = FALSE
 )
 
-search_tweets(q = "indonesia", n = 1, token = twitter_token)
-
 todayDate <- format(Sys.Date(), '%d %B %Y')
-todayDate <- '16 June 2022'
 query <- list(releaseDate = todayDate)
 
 releaseToday <- mongo_movie$find(toJSON(query, auto_unbox = T))
@@ -33,22 +31,15 @@ if(nReleaseToday > 0){
     title <- movie$name
     description <- movie$description
     url <- paste0('https://www.imdb.com', movie$url)
-    actor <- paste(movie$actor[[1]]$name, collapse = ', ')
-    genre <- paste( movie$genre[[1]], collapse = ', ')
     
     cat('Preparing to post to twitter:', title, '\n')
     
     cat('Composing status text', '\n')
-    text <- sprintf('[%s]\n%s\n%s\nActors: %s\nGenre: %s\n%s', todayDate, title, description, actor, genre, url) 
-    
-    cat('Downloading movie poster', '\n')
-    imageUrl <- movie$image
-    download.file(imageUrl,'image.jpg', mode = 'wb')
+    text <- sprintf('%s (%s): %s\n%s', toupper(title), todayDate, description, url) 
     
     cat('Posting to twitter', '\n')
     rtweet::post_tweet(
       status = text,
-      #media = 'image.jpg',
       token = twitter_token
     )
     
