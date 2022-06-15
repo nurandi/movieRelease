c450327b3efe01285e55d18f84d25fd6b4f5becb
Nur Andi Setiabudi <nurandi@users.noreply.github.com>
2022-06-16 05:26:59 +0700

    remove date

--- tweet.R
+++ tweet.R
@@ -17,7 +17,6 @@ twitter_token <- rtweet::create_token(
 )
 
 todayDate <- format(Sys.Date(), '%d %B %Y')
-todayDate <- '16 June 2022'
 query <- list(releaseDate = todayDate)
 
 releaseToday <- mongo_movie$find(toJSON(query, auto_unbox = T))
